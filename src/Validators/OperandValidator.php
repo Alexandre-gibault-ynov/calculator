6872e2aebc8b95ceb56755256dafb880db594758
Alexandre Gibault <alexandre.gibault@ynov.com>
2024-11-16 11:52:08 +0100

    [FEAT][VALIDATORS] Add operand validator class implementation

--- src/Validators/OperandValidator.php
+++ src/Validators/OperandValidator.php
@@ -4,7 +4,22 @@ declare(strict_types=1);
 
 namespace App\Validators;
 
+use InvalidArgumentException;
+
 class OperandValidator
 {
-
+    /**
+     * Validate an operation operand by testing if it's value is numeric.
+     * Return the operand int value.
+     *
+     * @param string $operand The operand to validate
+     * @return int The operand int value
+     * @throws InvalidArgumentException If the operand is not numeric
+     */
+    public static function validate(string $operand): int {
+        if (!is_numeric($operand)) {
+            throw new InvalidArgumentException("Invalid operand: $operand");
+        }
+        return (int)$operand;
+    }
 }
