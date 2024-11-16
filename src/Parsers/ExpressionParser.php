9d4618f9c85a4406628cb77e79c17f572dc8da20
Alexandre Gibault <alexandre.gibault@ynov.com>
2024-11-16 11:40:36 +0100

    [FEAT][PARSERS] Add expression parser class implementation

--- src/Parsers/ExpressionParser.php
+++ src/Parsers/ExpressionParser.php
@@ -5,8 +5,29 @@ declare(strict_types = 1);
 namespace App\Parsers;
 
 use App\OperatorEnum;
+use InvalidArgumentException;
 
-class ExpressionParser
+final class ExpressionParser
 {
+    public function parse(array $expression): array
+    {
+        if (count($expression) < 3 || count($expression) % 2 === 0) {
+            throw new \InvalidArgumentException("Invalid expression format.");
+        }
 
+        $parsedExpression = [];
+        foreach ($expression as $index => $element) {
+            if ($index % 2 === 0) {
+                $parsedExpression[] = ['type' => 'operand', 'value' => $element];
+            } else {
+                $operator = OperatorEnum::tryFrom($element);
+                if ($operator === null) {
+                    throw new InvalidArgumentException("Invalid operator: $element");
+                }
+                $parsedExpression[] = ['type' => 'operator', 'value' => $operator];
+            }
+        }
+
+        return $parsedExpression;
+    }
 }
