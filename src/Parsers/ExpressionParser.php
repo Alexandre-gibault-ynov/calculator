db9068531488e08767656c45dfde395bb6f43ca1
Alexandre Gibault <alexandre.gibault@ynov.com>
2024-11-16 11:58:05 +0100

    [FEAT][PARSERS] Add expression parser documentation

--- src/Parsers/ExpressionParser.php
+++ src/Parsers/ExpressionParser.php
@@ -9,6 +9,13 @@ use InvalidArgumentException;
 
 final class ExpressionParser
 {
+    /**
+     * Parse an operation expression into operands and operators.
+     *
+     * @param string[] $expression The operation expression.
+     * @return array<int, array{type: string, value: string}> Parsed operation expression.
+     * @throws InvalidArgumentException The expression is invalid.
+     */
     public function parse(array $expression): array
     {
         if (count($expression) < 3 || count($expression) % 2 === 0) {
