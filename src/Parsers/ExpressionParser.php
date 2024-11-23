dd8e74f0c6a7656adf2390dd65a942acbea8f40e
Alexandre Gibault <alexandre.gibault@ynov.com>
2024-11-23 15:36:36 +0100

    [FEAT][PARSERS] Update the expression parser to get a postfix expression

--- src/Parsers/ExpressionParser.php
+++ src/Parsers/ExpressionParser.php
@@ -10,7 +10,7 @@ use InvalidArgumentException;
 final class ExpressionParser
 {
     /**
-     * Parse an operation expression into operands and operators.
+     * Parse an operation expression into a postfix expression.
      *
      * @param string[] $expression The operation expression.
      * @return array<int, array{type: string, value: string}> Parsed operation expression.
@@ -19,22 +19,55 @@ final class ExpressionParser
     public function parse(array $expression): array
     {
         if (count($expression) < 3 || count($expression) % 2 === 0) {
-            throw new \InvalidArgumentException("Invalid expression format.");
+            throw new InvalidArgumentException("Invalid expression format.");
         }
 
-        $parsedExpression = [];
-        foreach ($expression as $index => $element) {
-            if ($index % 2 === 0) {
-                $parsedExpression[] = ['type' => 'operand', 'value' => $element];
-            } else {
-                $operator = OperatorEnum::tryFrom($element);
-                if ($operator === null) {
-                    throw new InvalidArgumentException("Invalid operator: $element");
+        $postfixExpression = [];
+        $operators = [];
+
+        foreach ($expression as $element) {
+            $is_operand = is_numeric($element);
+
+            if ($is_operand) {
+                $postfixExpression[] = $element;
+            } else if ($operator = OperatorEnum::tryFrom($element)) {
+
+                while (!empty($operators) && $this->hasHigherPrecedence(end($operators), $operator)) {
+                    $postfixExpression[] = array_pop($operators);
                 }
-                $parsedExpression[] = ['type' => 'operator', 'value' => $operator];
+                $operators[] = $operator;
+
+            } else {
+                throw new InvalidArgumentException("Invalid Token: $element");
             }
         }
 
-        return $parsedExpression;
+        while (!empty($operators)) {
+            $postfixExpression[] = array_pop($operators);
+        }
+
+        return $postfixExpression;
     }
+
+    /**
+     * Return true if the first operator has a higher priority than the second operator
+     * else false.
+     *
+     * @param OperatorEnum $operator1 The first operator.
+     * @param OperatorEnum $operator2 The second operator.
+     * @return bool True if the first operator has a higher priority than the second operator.
+     */
+    private function hasHigherPrecedence(OperatorEnum $operator1, OperatorEnum $operator2): bool
+    {
+        $precedence = [
+            OperatorEnum::ADD->value => 1,
+            OperatorEnum::SUBTRACT->value => 1,
+            OperatorEnum::MULTIPLY->value => 2,
+            OperatorEnum::DIVIDE->value => 2,
+        ];
+
+        return $precedence[$operator1->value] >= $operator2[$operator2->value];
+    }
+
+
 }
