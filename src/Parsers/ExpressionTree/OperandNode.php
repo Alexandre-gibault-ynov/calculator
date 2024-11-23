f7b1282df461c2a79de7b501d72608e694eeef69
Alexandre Gibault <alexandre.gibault@ynov.com>
2024-11-23 15:18:02 +0100

    [FEAT][PARSERS] tree expression implementation

---
+++ src/Parsers/ExpressionTree/OperandNode.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Parsers\ExpressionTree;
+
+class OperandNode implements NodeInterface
+{
+
+    /**
+     * @var int The operand's value
+     */
+    private int $value;
+
+    public function __construct(int $value){
+        $this->value = $value;
+    }
+
+    #[\Override] public function evaluate(): int
+    {
+        return $this->value;
+    }
+}
---
+++ src/Parsers/ExpressionTree/OperationNode.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Parsers\ExpressionTree;
+
+use App\Operations\OperationInterface;
+use App\Parsers\ExpressionTree;
+
+class OperationNode implements NodeInterface
+{
+    /**
+     * @var OperationInterface The operation of the node.
+     */
+    private OperationInterface $operation;
+
+    /**
+     * @var NodeInterface Left expression of the node.
+     */
+    private NodeInterface $left;
+
+    /**
+     * @var NodeInterface Right expression of the node.
+     */
+    private NodeInterface $right;
+
+    public function __construct(OperationInterface $operation, NodeInterface $left, NodeInterface $right) {
+        $this->operation = $operation;
+        $this->left = $left;
+        $this->right = $right;
+    }
+
+    #[\Override] public function evaluate(): int
+    {
+        return $this->operation->calculate(
+            $this->left->evaluate(),
+            $this->right->evaluate()
+        );
+    }
+}
