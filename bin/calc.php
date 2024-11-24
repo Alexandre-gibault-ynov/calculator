05e71f4ae6b0d23680155b1adf57fa82f4f612b6
Alexandre Gibault <alexandre.gibault@ynov.com>
2024-11-24 15:25:33 +0100

    [FEAT][FILE READING] Add file reader usage into the app

--- bin/calc.php
+++ bin/calc.php
@@ -5,17 +5,27 @@ declare(strict_types=1);
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use App\Calculator;
+use App\FileReader;
 
-if ($argc < 4) {
-    echo "Usage: php calc.php <number> <operator> <number> [...]\n";
+if ($argc !== 2) {
+    echo "Usage: php calc.php <file_path>\n";
     exit(1);
 }
 
-$expression = array_slice($argv, 1);
+$filePath = $argv[1];
 
-$calculator = new Calculator();
+$fileReader = new FileReader();
 try {
-    echo $calculator->calculate($expression);
+    $expression = $fileReader->readExpression($filePath);
+
+    $calculator = new Calculator();
+    $result = $calculator->calculate($expression) . "\n";
+
+    foreach ($expression as $token) {
+        echo $token . " ";
+    }
+    echo "= " . $result . "\n";
+
 } catch (Exception $e) {
     echo "Error : " . $e->getMessage() . "\n";
     exit(1);
